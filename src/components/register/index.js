f4efaa8186fef3d017ff352eaf2de95a198f7fec
Dominic Oludare <dominicoludare@gmail.com>
2023-01-23 02:26:45 +0100

    Folder and files creation

---
+++ src/components/register/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './index.scss'
+
+const RegistrationPage =()=> {
+
+    return (
+        <div>
+            <div>
+                <label>First name</label>
+                <input type="text" />
+                <label>Email Address</label>
+                <input type="text" />
+                <label>Phone Number</label>
+                <input type="text" />
+                <label>Password</label>
+                <input type="text" />
+            </div>
+        </div>
+    );
+}
+
+export default RegistrationPage
---
+++ src/components/verification/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+
+const VerificationPage =()=> {
+
+    return (
+        <div>
+            <h3>Kindly enter Email verfication code</h3>
+        </div>
+    );
+}
+
+export default VerificationPage;
